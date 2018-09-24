496e02f158309bd472be9f1a5cf0a2a1920484f9
Misha Bergal <misha.bergal@gmail.com>
2018-09-24 12:19:46 -0500

    Complex process

--- __tests__/dsl.test.ts
+++ __tests__/dsl.test.ts
@@ -1,4 +1,4 @@
-import { process, seq, run } from "../dsl";
+import { process, seq, par, run } from "../dsl";
 
 describe("DSL", () => {
   it("seq", async done => {
@@ -6,11 +6,37 @@ describe("DSL", () => {
       process(async () => Promise.resolve(1)),
       process(async (x: number) => Promise.resolve(x + 1))
     );
-    expect(await run(p, {}, 0)).toEqual(2);
+    expect(await run(p, "", {}, 0)).toEqual(2);
+    done();
+  });
+
+  it("par", async done => {
+    const p = par(
+      process(async () => Promise.resolve(1)),
+      process(async () => Promise.resolve(2))
+    );
+    expect(await run(p, "", {}, 0)).toEqual([1, 2]);
+    done();
+  });
+
+  fit("complex", async done => {
+    const p = seq(
+      seq(
+        process(() => Promise.resolve(1)),
+        par<number, number, string>(
+          process(x => Promise.resolve(x + 1)),
+          process(x => Promise.resolve(x.toString()))
+        )
+      ),
+      process((x: [number, string]) => Promise.resolve(x))
+    );
+
+    const r = await run(p, "", {}, 1);
+    console.log(r);
     done();
   });
   it("correctly uses state", async done => {
-    const calls: string[] = [];
+    let calls: string[] = [];
     const p = seq(
       process(async () => {
         calls.push("1");
@@ -22,9 +48,11 @@ describe("DSL", () => {
       })
     );
     const state = {};
-    expect(await run(p, state, 0)).toEqual(2);
+    expect(await run(p, "", state, 0)).toEqual(2);
     expect(calls).toEqual(["1", "2"]);
-    expect(await run(p, state, 0)).toEqual(2);
+    calls = [];
+    expect(await run(p, "", state, 0)).toEqual(2);
+    expect(calls).toEqual([]);
     done();
   });
 });
--- dsl.ts
+++ dsl.ts
@@ -1,17 +1,23 @@
 export interface Process<T, R> {
   id: string;
-  f: (v: T, state: {}) => Promise<R>;
+  f: (v: T, id: string, state: {}) => Promise<R>;
 }
 
 export function process<T, R>(f: ((v: T) => Promise<R>)): Process<T, R> {
   return {
     id: "",
-    f: f
+    async f(v: T, id: string, state: { [id: string]: any }) {
+      if (state[id] === undefined) {
+        console.log(`Running ${id}`);
+        state[id] = await f(v);
+      }
+      return state[id];
+    }
   };
 }
 
-export async function run<T, R>(p: Process<T, R>, state: {}, i: T) {
-  const r = await p.f(i, state);
+export async function run<T, R>(p: Process<T, R>, id: string, state: {}, i: T) {
+  const r = await p.f(i, id, state);
   return r;
 }
 
@@ -21,15 +27,36 @@ export function seq<T1, T2, R>(
 ): Process<T1, R> {
   return {
     id: "seq",
-    async f(v: T1, state: {}): Promise<R> {
-      const r1 = await p1.f(v, state);
-      const r2 = await p2.f(r1, state);
-      return r2;
+    async f(v: T1, id: string, state: { [id: string]: any }): Promise<R> {
+      const thisProcessId = `${id}.seq`;
+      if (state[thisProcessId] === undefined) {
+        console.log(`Running ${thisProcessId}`);
+        const r1 = await p1.f(v, `${thisProcessId}.1`, state);
+        const r2 = await p2.f(r1, `${thisProcessId}.2`, state);
+        state[thisProcessId] = r2;
+      }
+      return state[thisProcessId];
     }
   };
 }
 
-const p = seq(
-  process(async () => Promise.resolve(1)),
-  process(async (x: number) => Promise.resolve(x + 1))
-);
+export function par<T, R1, R2>(
+  p1: Process<T, R1>,
+  p2: Process<T, R2>
+): Process<T, [R1, R2]> {
+  return {
+    id: "par",
+    async f(v: T, id: string, state: { [id: string]: any }): Promise<[R1, R2]> {
+      const thisProcessId = `${id}.par`;
+      if (state[thisProcessId] === undefined) {
+        console.log(`Running ${thisProcessId}`);
+        const [r1, r2] = await Promise.all([
+          p1.f(v, `${thisProcessId}.1`, state),
+          p2.f(v, `${thisProcessId}.2`, state)
+        ]);
+        state[thisProcessId] = [r1, r2];
+      }
+      return state[thisProcessId];
+    }
+  };
+}
