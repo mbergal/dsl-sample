d43e6b766e5c80fe3154a75154f16f95359c0fb4
Misha Bergal <misha@misha33.local>
2018-09-25 12:19:18 -0500

    Cleanup

--- final-tagless.ts
+++ final-tagless.ts
@@ -1,8 +1,11 @@
-interface ExecutingProcess<T, R> {}
-
+// Brand is used to distinguish
+//
 interface IProcess<Brand, T, R> {
   // Make interface nominal
   _nominal(tag: [Brand, T, R]): void;
+
+  // val inj : ’a list → (’a, t) app
+  // val prj : (’a, t) app → ’a list
 }
 
 // Algebra
@@ -16,74 +19,92 @@ interface IProcessAlg<Brand> {
 }
 
 namespace Execution {
-export class Brand {
-  _executionBrand: null;
-}
-
-interface ExecutingProcess<T, R> extends IProcess<Brand, T, R> {
-}
-
-export class Factory implements IProcessAlg<Brand> {
-  _nominal(tag: [Brand]): void {}
-  process<T, R>(f: (v: T) => R): IProcess<Brand, T, R> {
-    const process = <ExecutingProcess<T, R>>{ _nominal(tag: [Brand, T, R]) {} };
-    return process;
+  export class Brand {
+    _executionBrand: null;
+    static prj<T, R>(a: IProcess<Brand, T, R>): Process<T, R> {
+      return <Process<T, R>>a;
+    }
   }
 
-  seq<T, T1, R>(
-    p1: IProcess<Brand, T, T1>,
-    p2: IProcess<Brand, T1, R>
-  ): IProcess<Brand, T, R> {
-    return <ExecutingProcess<T, R>>{ _nominal(x) {}, id: "" };
+  interface Process<T, R> extends IProcess<Brand, T, R> {}
+
+  export class Factory implements IProcessAlg<Brand> {
+    _nominal(tag: [Brand]): void {}
+    process<T, R>(f: (v: T) => R): IProcess<Brand, T, R> {
+      const process = <Process<T, R>>{
+        _nominal(tag: [Brand, T, R]) {}
+      };
+      return process;
+    }
+
+    seq<T, T1, R>(
+      p1: IProcess<Brand, T, T1>,
+      p2: IProcess<Brand, T1, R>
+    ): IProcess<Brand, T, R> {
+      return <Process<T, R>>{ _nominal(x) {}, id: "" };
+    }
   }
 }
-}
 
-namespace Print {
-export class Brand {
-  _printBrand: null;
-}
-
-class ShowProcess<T, R> implements IProcess<Brand, T, R> {
-  value: string;
-  constructor(p: string) {
-    this.value = p;
-  }
-  _nominal(x: [Brand, T, R]) {}
-}
-
-export class Factory implements IProcessAlg< Brand> {
-  _nominal(tag: [Brand]): void {}
-
-  process<T, R>(f: (v: T) => R): IProcess< Brand, T, R> {
-    return new ShowProcess<T, R>("process");
+namespace Show {
+  export class Brand {
+    _printBrand: null;
+    static prj<T, R>(a: IProcess<Brand, T, R>): Process<T, R> {
+      return <Process<T, R>>a;
+    }
   }
 
-  seq<T, T1, R>(
-    p1: IProcess< Brand, T, T1>,
-    p2: IProcess< Brand, T1, R>
-  ): IProcess< Brand, T, R> {
-    const r1 = <ShowProcess<T, R>>(<any>p1);
-    const r2 = <ShowProcess<T, R>>(<any>p2);
-    return new ShowProcess<T, R>(`seq:\n  ${r1.value}\n  ${r2.value}`);
+  export class Process<T, R> implements IProcess<Brand, T, R> {
+    value: string;
+    constructor(p: string) {
+      this.value = p;
+    }
+    _nominal(x: [Brand, T, R]) {}
+  }
+
+  export class Factory implements IProcessAlg<Brand> {
+    _nominal(tag: [Brand]): void {}
+
+    process<T, R>(f: (v: T) => R): IProcess<Brand, T, R> {
+      return new Process<T, R>("process");
+    }
+
+    seq<T, T1, R>(
+      p1: IProcess<Brand, T, T1>,
+      p2: IProcess<Brand, T1, R>
+    ): IProcess<Brand, T, R> {
+      const r1 = <Process<T, R>>(<any>p1);
+      const r2 = <Process<T, R>>(<any>p2);
+      return new Process<T, R>(
+        `seq:\n${indent(4, r1.value)}\n${indent(4, r2.value)}`
+      );
+    }
   }
 }
+
+function pipeline<T>(alg: IProcessAlg<T>) {
+  return alg.seq(
+    alg.seq(
+      alg.process((x: string) => 1),
+      alg.process((x: number) => x.toString())
+    ),
+    alg.process((x: string) => parseInt(x))
+  );
 }
 
 const lf: IProcessAlg<Execution.Brand> = new Execution.Factory();
-let pf: IProcessAlg<Print.Brand> = new Print.Factory();
+let pf: IProcessAlg<Show.Brand> = new Show.Factory();
 
-const p1 = pf.process((x: string) => 1);
-
-let p2 = pf.process((x: number) => ({
-  x: x
-}));
-
-function value<T, R>(
-  v: IProcess<typeof PrintFactory, T, R>
-): ShowProcess<T, R> {
-  return <ShowProcess<T, R>>v;
+function value<T, R>(v: IProcess<Show.Brand, T, R>): Show.Process<T, R> {
+  return <Show.Process<T, R>>v;
 }
 
-const a = value(pf.seq(p1, p2));
+function indent(indent: number, str: string) {
+  const lines = str.split(/(\r?\n)/);
+  return lines.map(line => " ".repeat(indent) + line).join("");
+}
+
+const p = pipeline(new Show.Factory());
+const a = Show.Brand.prj(p);
+
 console.log(a.value);
